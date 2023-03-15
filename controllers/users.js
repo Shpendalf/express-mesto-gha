59babd242afb12cdb116395a39314c305af137d9
nikolay zheltov <shpendalf@gmail.com>
2023-03-15 03:03:47 +0300

    Lintfix

--- controllers/users.js
+++ controllers/users.js
@@ -1,25 +1,25 @@
 const Users = require('../models/user');
-const ErrorIsntFound = require ('../errors/ErrorIsntFound');
-const OtherError = require ('../errors/OtherError');
+const ErrorIsntFound = require('../errors/ErrorIsntFound');
+const OtherError = require('../errors/OtherError');
 const ValidationError = require('../errors/ValidationError');
-const { ErrorHandler } = require('../middlewares/ErrorHandler')
+const { ErrorHandler } = require('../middlewares/ErrorHandler');
+
 module.exports.getUsers = (req, res) => {
   Users.find({})
     .then((user) => res.send(user))
-    .catch((error) => {ErrorHandler(error, res)})
+    .catch((error) => { ErrorHandler(error, res); });
 };
 
 module.exports.getUserById = (req, res) => {
   Users.findById(req.params.userId)
     .then((user) => {
       if (!user) throw new ErrorIsntFound();
-      else
-      return res.status(200).send(user);
+      else return res.status(200).send(user);
     })
     .catch((error) => {
-      ErrorHandler(error, res,{
-        valMessage:"Переданы некорректные данные профиля"
-      },);
+      ErrorHandler(error, res, {
+        valMessage: 'Переданы некорректные данные профиля',
+      });
     });
 };
 
@@ -28,11 +28,11 @@ module.exports.createUser = (req, res) => {
   Users.create({ name, about, avatar })
     .then((user) => { res.status(201).send(user); })
     .catch((error) => {
-      ErrorHandler(error, res,{
-        valMessage:"Передан неверный формат ID",
-        isntFoundMessage:"Пользователь не найден",
+      ErrorHandler(error, res, {
+        valMessage: 'Передан неверный формат ID',
+        isntFoundMessage: 'Пользователь не найден',
 
-      },);
+      });
     });
 };
 
@@ -41,17 +41,15 @@ module.exports.updateProfile = (req, res) => {
 
   Users.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true, upsert: false })
     .then((user) => {
-      if(!name || !about) throw new ValidationError();
+      if (!name || !about) throw new ValidationError();
       else if (!user) throw new ErrorIsntFound();
-      else
-      res.status(200).send(user);
+      else res.status(200).send(user);
     })
     .catch((err) => {
-      ErrorHandler(err,res,{
-        valMessage:"Переданы неверные данные пользователя",
-        isntFoundMessage:"Пользователь не найден",
-      }
-      )
+      ErrorHandler(err, res, {
+        valMessage: 'Переданы неверные данные пользователя',
+        isntFoundMessage: 'Пользователь не найден',
+      });
     });
 };
 
@@ -60,17 +58,15 @@ module.exports.updateAvatar = (req, res) => {
   const { avatar } = req.body;
 
   Users.findByIdAndUpdate(owner, { avatar }, { new: true, runValidators: true, upsert: false })
-  .then((user) => {
-  if (!avatar) throw new ValidationError();
-   else if(!user) throw new ErrorIsntFound();
-   else res.status(200).send(user)
+    .then((user) => {
+      if (!avatar) throw new ValidationError();
+      else if (!user) throw new ErrorIsntFound();
+      else res.status(200).send(user);
     })
     .catch((err) => {
-     ErrorHandler(
-      err,res, {
+      ErrorHandler(err, res, {
         isntFoundMessage: 'Пользователь не найден',
         valMessage: 'Переданы неверные данные пользователя',
-      },
-     );
+      });
     });
 };
